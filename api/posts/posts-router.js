98b3531491b4fb2599a8b38622b2504e84fdb6c8
Fernando Martinez <fernando817mm@gmail.com>
2021-08-31 14:42:38 -0500

    finished mvp

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,122 @@
 // implement your posts router here
+const Post = require('./posts-model');
+
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    Post.find()
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(err => res.status(500).json({
+            message: `The posts information could not be retrieved`,
+            error: err.message
+        }))
+})
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    Post.findById(id)
+        .then(post => {
+            (!post)
+            ? res.status(404).json({
+                message: `The post with the specified ID does not exist`
+            })
+            : res.status(200).json(post);
+        })
+        .catch(err => res.status(500).json({
+            error: err.message
+        }))
+})
+
+router.post('/', (req, res) => {
+    const newPost = req.body;
+    const { title, contents } = newPost;
+    (!title || !contents)
+    ? res.status(400).json({
+        message: `Please provide title and contents for the post`
+    })
+    : Post.insert(newPost)
+        .then(post => {
+            const { id } = post;
+            Post.findById(id)
+                .then(post => res.status(201).json(post))
+                .catch(err => res.status(500).json({ message: err.message }))
+        })
+        .catch(err => res.status(500).json({
+            message: `There was an error while saving the post to the database`,
+            error: err.message
+        }))
+})
+
+router.put('/:id', (req, res) => {
+    const updatedPost = req.body;
+    const { title, contents } = updatedPost;
+    const { id } = req.params;
+
+    (!title || !contents)
+    ? res.status(400).json({
+        message: `Please provide title and contents for the post`
+    })
+    : Post.findById(id)
+        .then(post => {
+            (!post)
+            ? res.status(404).json({
+                message: `The post with the specified ID does not exist`
+            })
+            : Post.update(id, updatedPost)
+                .then(post => {
+                    Post.findById(id - post + 1)
+                        .then(post => {
+                            res.status(200).json(post);
+                        })
+                        .catch(err => res.status(500).json(err))
+                })
+                .catch(err => res.status(500).json({
+                    message: `The post information could not be modified`,
+                    error: err.message
+                }))
+        })
+        .catch(err => res.status(500).json({ message: err.message }))
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    Post.findById(id)
+        .then(deletedPost => {
+            (!deletedPost)
+            ? res.status(404).json({
+                message: `The post with the specified ID does not exist`
+            })
+            : Post.remove(id)
+                .then(post => {
+                    (!post)
+                    ? res.status(500).json({message: `Something is wrong`})
+                    : res.status(200).json(deletedPost);
+                })
+                .catch(err => res.status(500).json({
+                    message: `The post could not be removed`,
+                    error: err.message
+                }))
+        })
+        .catch(err => res.status(500).json({ message: err.message }))
+})
+
+router.get('/:id/comments', (req, res) => {
+    const { id } = req.params;
+    Post.findById(id)
+        .then(post => {
+            (!post)
+            ? res.status(404).json({
+                message: `The post with the specified ID does not exist`
+            })
+            : Post.findPostComments(id)
+                .then(comments => {
+                    res.status(200).json(comments);
+                })
+                .catch(err => res.status(500).json({ message: `The comments information could not be retrieved`, error: err.message }))
+        })
+
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,19 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+const postsRouter = require('./posts/posts-router');
+
+server.use('/api/posts', postsRouter);
+
+server.get('*', (req, res) => {
+    res.status(404).send(`
+      <h2>Lambda Shelter API</h>
+      <p>Oops, we cannot find that resource</p>
+    `);
+});
+  
+module.exports = server;
